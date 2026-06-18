4947628cf67bd6ce3c8d936ada8b093e2ae5c122
leticia Vitoria <leticiafirmino2005@gmal.com>
2026-06-17 21:44:35 -0400

    Adicionando nova página para cada urls

--- livros/urls.py
+++ livros/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('meus-livros/', views.meus_livros, name='meus_livros'),
+    path('favoritos/', views.favoritos, name='favoritos'),
+    path('metas/', views.metas, name='metas'),
 ]
--- livros/views.py
+++ livros/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def meus_livros(request):
+    # TODO: buscar livros do banco de dados
+    livros = []
+    context = {'livros': livros}
+    return render(request, 'meus_livros.html', context)
+
+def favoritos(request):
+    # TODO: buscar livros favoritos do banco de dados
+    favoritos = []
+    context = {'favoritos': favoritos}
+    return render(request, 'favoritos.html', context)
+
+def metas(request):
+    # TODO: buscar metas do banco de dados
+    metas = []
+    context = {'metas': metas}
+    return render(request, 'metas.html', context)
