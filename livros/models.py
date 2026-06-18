a92589959fda991c4de22b24efaffa5e572c7b6f
amizael_ygor <amizaelcavalcante245@gmail.com>
2026-06-17 21:33:42 -0400

    bootstrap inserido e codigo atualizado

---
+++ livros/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Livro(models.Model):
+
+    STATUS = (
+        ('QUERO_LER', 'Quero Ler'),
+        ('LENDO', 'Lendo'),
+        ('CONCLUIDO', 'Concluído'),
+        ('ABANDONADO', 'Abandonado'),
+    )
+
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=150)
+    genero = models.CharField(max_length=100)
+    ano = models.IntegerField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS,
+        default='QUERO_LER'
+    )
+
+    progresso = models.IntegerField(default=0)
+    nota = models.IntegerField(default=0)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
